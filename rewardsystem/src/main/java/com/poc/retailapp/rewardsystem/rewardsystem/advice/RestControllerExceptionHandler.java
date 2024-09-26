fa5eb144d2564b889818f0b89a0d8197935eacd2
sujeet kumar <73944425+sujeet-jaiswal@users.noreply.github.com>
2024-09-26 23:07:01 +0530

    Update RestControllerExceptionHandler.java
    removing redundant exception endpoints

--- rewardsystem/src/main/java/com/poc/retailapp/rewardsystem/rewardsystem/advice/RestControllerExceptionHandler.java
+++ rewardsystem/src/main/java/com/poc/retailapp/rewardsystem/rewardsystem/advice/RestControllerExceptionHandler.java
@@ -16,20 +16,9 @@ public class RestControllerExceptionHandler {
     @ExceptionHandler(value = RewardServiceException.class)
     public ResponseEntity<?> handleRewardServiceException(
             RuntimeException e) {
-        log.error("Error occurred: {}", e.getLocalizedMessage(), e);
+      
         return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(
-            RuntimeException e) {
-        log.error("Error occurred: {}", e.getLocalizedMessage(), e);
-        return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<?> handleGlobalException(Exception e) {
-        log.error("Internal Server Error {}", e.getLocalizedMessage(), e);
-        return new ResponseEntity<>("System Error. Please contact support", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  
 }
